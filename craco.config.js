40238f959571616c6fdfdf5f5ceb9a313e3b7837
Sandro Roth <sandroroth@outlook.com>
2022-09-03 18:32:30 +0200

    Configure ModuleScopePlugin for production build

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,5 @@
 const { VanillaExtractPlugin } = require("@vanilla-extract/webpack-plugin");
+const ModuleScopePlugin = require("react-dev-utils/ModuleScopePlugin");
 const cloneDeep = require("lodash.clonedeep");
 const path = require("path");
 
@@ -44,6 +45,13 @@ module.exports = {
       // it must come before the existing CSS loader which
       // handles all CSS files.
       oneOfRule.oneOf.unshift(vanillaExtractRule);
+
+      const moduleScopePlugin = webpackConfig.resolve.plugins.find(
+        (plugin) => plugin instanceof ModuleScopePlugin
+      );
+      moduleScopePlugin.allowedPaths.push(
+        path.resolve(__dirname, "node_modules/@vanilla-extract/webpack-plugin")
+      );
 
       return webpackConfig;
     },
